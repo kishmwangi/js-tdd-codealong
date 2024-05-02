e3feaac47cce4ff6e56e0fbbdd64a34fdf8fed24
kishmwangi <kishmwangi31@gmail.com>
2024-05-02 07:29:00 +0300

    js-tdd

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,3 @@
-import { currentAgeForBirthYear } from "../utils";
-
-describe("currentAgeForBirthYear", () => {
-  it("returns the age of a person based on the year of birth", () => {
-    const birthYear = 1984;
-    const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
-  });
-});
+export function currentAgeBirthYear(birthYear) {
+  return new Date().getFullYear() -birthYear;
+}
